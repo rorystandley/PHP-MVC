bbce669f0515a55354ddb40014abfdcff27412a1
Rory Standley <rorystandley@gmail.com>
2013-11-30 23:25:48 +0000

    Added .htaccess file
    This file is needed to route everything through the index.php file

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ define('ROOT', dirname(dirname(__FILE__)));
 
  // Our index.php basically sets the $url variable and calls bootstrap.php which resides in our library directory.
 $url = $_GET['url'];
- 
+
 require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
