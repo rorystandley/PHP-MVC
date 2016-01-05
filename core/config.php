3c29619340b7bfd27e57c26964b29a802d745f70
Rory Standley <rorystandley@gmail.com>
2016-01-05 12:18:45 +0000

    Feature - Dotenv now required
    This has removed much unneeded code that is handled more efficiently
    with dotenv

--- core/config.php
+++ core/config.php
@@ -2,20 +2,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-if ( $_SERVER['SERVER_NAME'] == 'PHP-MVC.dev') {
-	$env = 'development';
-}
-
-$json = json_decode(file_get_contents( '../database.json' ), true);
-$dbhost = isset($json[$env]["host"]) ? $json[$env]["host"] : "";
-$dbhost = isset($json[$env]["port"]) ? $dbhost.":".$json[$env]["port"] : $dbhost;
-$dbuser = isset($json[$env]["user"]) ? $json[$env]["user"] : "";
-$dbpass = isset($json[$env]["password"]) ? $json[$env]["password"] : "";
-$dbname = isset($json[$env]["database"]) ? $json[$env]["database"] : "";
-
 $dbCreds = array(
-	"db_host" => $dbhost,
-	"db_user" => $dbuser,
-	"db_pass" => $dbpass,
-	"db_name" => $dbname
+	"db_host" => getenv('DB_HOST'),
+	"db_user" => getenv('DB_USER'),
+	"db_pass" => getenv('DB_PASS'),
+	"db_name" => getenv('DB_NAME')
 );
--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,10 @@
 <?php
-$env = 'live';
+require '../vendor/autoload.php';
+// Lets add in our .env magic
+$dotenv = new Dotenv\Dotenv('../');
+// We need specific values in the file, so lets check we have them
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_PORT', 'DB_USER', 'DB_PASS']);
+$dotenv->required('DB_NAME')->notEmpty();
+// Now lets add in our application
 include("../core/application.php");
